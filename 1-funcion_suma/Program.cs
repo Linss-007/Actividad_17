b5e400f512d6a141b0072422378e531569520a90
Crist <Crist@TILIN>
2026-03-12 11:32:28 -0600

    Agregar archivos de proyecto.

---
+++ 1-funcion_suma/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Ingrese el primer numero");
+double num1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el segundo numero");
+double num2 = double.Parse(Console.ReadLine());
+Console.WriteLine("El resultado de la suma es de: " + suma(num1,num2));
+
+
+
+
+double suma(double num1, double num2)
+{
+    return num1 + num2;
+}
