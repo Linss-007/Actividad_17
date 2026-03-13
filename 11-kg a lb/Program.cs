9362b88cb46c2336f0f87cb98b91ed42ffd31c86
Linss-007 <cristopheradl7@gmail.com>
2026-03-12 23:03:53 -0600

    Ejercicio 11

---
+++ 11-kg a lb/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Ingrese los kilogramos a convertir");
+double kilog = double.Parse(Console.ReadLine());
+Console.WriteLine($"El resultado en libras es de: {kilosgLibra(kilog)}");
+
+
+double kilosgLibra(double kilog)
+{
+    return kilog * 2.205;
+}
