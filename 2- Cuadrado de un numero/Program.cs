d10eece3a73603c2afa0f236165e8706602f43d5
Linss-007 <cristopheradl7@gmail.com>
2026-03-12 21:34:07 -0600

    Segundo ejercicio

---
+++ 2- Cuadrado de un numero/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Ingrese numero a sacar cuadrado");
+double numero = double.Parse(Console.ReadLine());
+Console.WriteLine("El resultado del cuadrado es de: " + cuadrado(numero));
+
+
+
+double cuadrado(double numero)
+{
+    return numero * numero;
+}
