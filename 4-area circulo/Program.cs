0a3c2b6142bd116390eeae17d0bc3388d30707e5
Linss-007 <cristopheradl7@gmail.com>
2026-03-12 22:21:44 -0600

    Cuarto ejercicio

---
+++ 4-area circulo/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Ingrese radio del circulo a calcular area");
+double radio = double.Parse(Console.ReadLine());
+Console.WriteLine($"El area del circulo es de: {areaCirculo(radio)}");
+
+
+double areaCirculo(double radio)
+{
+    return 3.1416 * (radio * radio);
+}
