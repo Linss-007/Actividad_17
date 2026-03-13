64bea5f3e92d33b92a578ade3abedd73d151c213
Linss-007 <cristopheradl7@gmail.com>
2026-03-12 22:54:12 -0600

    Ejercicio 9

---
+++ 9-Salario/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Ingrese las horas trabajadas");
+double horas = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el pago por hora");
+double pagoHora = double.Parse(Console.ReadLine());
+Console.WriteLine($"El total del pago es de: {salario(horas,pagoHora)}");
+
+double salario(double horas, double pagoHora)
+{
+    return horas * pagoHora;
+}
