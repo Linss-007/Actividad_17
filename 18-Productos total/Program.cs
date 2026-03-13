b11774e2f5e1f82e9160cfa71f7c8da6b94c0cd7
Linss_007 <cristopheradl7@gmail.com>
2026-03-13 09:42:18 -0600

    Ejercicio 18

---
+++ 18-Productos total/Program.cs
@@ -0,0 +1,19 @@
+﻿double totalPagar = 0;
+Console.WriteLine("Ingrese la cantidad de productos");
+int cantProducto = int.Parse(Console.ReadLine());
+pedirProducto(cantProducto);
+Console.WriteLine($"El total a pagar es de: {totalPagar}");
+
+
+
+
+
+void pedirProducto(int cantProducto)
+{
+    for (int i = 1; i <= cantProducto; i++)
+    {
+        Console.WriteLine($"Ingrese el precio del producto {i}");
+        double precio = double.Parse(Console.ReadLine());
+        totalPagar += precio;
+    }
+}
