eb5c9799eaea020c0a99de183568daceb0394cd7
Linss-007 <cristopheradl7@gmail.com>
2026-03-12 22:36:43 -0600

    Ejercicio 7

---
+++ 7-Descuento/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Ingrese precio del producto");
+double precio = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el porcentaje de descuento del producto");
+double descuento = double.Parse(Console.ReadLine());
+Console.WriteLine($"El total con el descuento aplicado es de: {totalDesc(precio,descuento)}");
+
+
+
+double totalDesc(double precio, double descuento)
+{
+    return precio - (precio * (descuento / 100));
+}
