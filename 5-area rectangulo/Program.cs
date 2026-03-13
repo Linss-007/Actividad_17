aad4883edf752c76b38079499a79f8c1c9e8cafd
Linss-007 <cristopheradl7@gmail.com>
2026-03-12 22:25:14 -0600

    Ejercicio 5

---
+++ 5-area rectangulo/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Ingrese la base del reactangulo");
+double baseRec = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese la altura del rectangulo");
+double alturaRec = double.Parse(Console.ReadLine());
+Console.WriteLine($"El area del rectangulo es de: {areaRec(alturaRec, baseRec)}");
+
+
+double areaRec(double baseRec, double alturaRec)
+{
+    return baseRec * alturaRec;
+}
