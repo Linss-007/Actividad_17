13fb172e659f635612fbf0f9e3040760497492fc
Linss_007 <cristopheradl7@gmail.com>
2026-03-13 09:58:29 -0600

    Ejercicio 19

---
+++ 19-nombre y calificación/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Ingrese su nombre");
+string nombre = Console.ReadLine();
+Console.WriteLine("Ingrese la calificacion 1");
+double calif1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese la calificacion 2");
+double calif2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese la calificacion 3");
+double calif3 = double.Parse(Console.ReadLine());
+Console.WriteLine($"hola {nombre} el promedio de las calificaciones es de: {promCalif(calif1,calif2,calif3)}");
+
+
+
+
+double promCalif(double calif1, double calif2, double calif3)
+{
+    return (calif1 + calif2 + calif3) / 3;
+}
