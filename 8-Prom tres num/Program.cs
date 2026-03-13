454547cea878961331f99ed1f025b8c5ad70e9cd
Linss-007 <cristopheradl7@gmail.com>
2026-03-12 22:41:13 -0600

    Ejercicio 8

---
+++ 8-Prom tres num/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Ingrese el primer numero");
+double num1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el segundo numero");
+double num2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el tercer numero");
+double num3 = double.Parse(Console.ReadLine());
+Console.WriteLine($"El resultado del promedio de los numeros es de: {promedio(num1,num2,num3)}");
+
+
+
+double promedio(double num1, double num2, double num3)
+{
+    return (num1 + num2 + num3) / 3;
+}
