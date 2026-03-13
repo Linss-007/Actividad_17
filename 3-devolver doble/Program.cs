d0d24a6eade114fbeb8d36ef03b383e7b7facf2f
Linss-007 <cristopheradl7@gmail.com>
2026-03-12 21:45:21 -0600

    Tercer ejercicio

---
+++ 3-devolver doble/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Ingrese numero a sacar doble");
+double numero = double.Parse(Console.ReadLine());
+Console.WriteLine($"El resultado del doble es de: {doble(numero)}");
+
+
+double doble(double numero)
+{
+    return numero * 2;
+}
