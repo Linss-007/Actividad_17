e6f93253b1a0edb269dec4e2e4625220b420896c
Linss_007 <cristopheradl7@gmail.com>
2026-03-13 08:27:24 -0600

    Ejercicio 13

---
+++ 13-Productos total/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Ingrese el precio de 3 productos");
+double pro1 = double.Parse(Console.ReadLine());
+double pro2 = double.Parse(Console.ReadLine());
+double pro3 = double.Parse(Console.ReadLine());
+Console.WriteLine($"El total a pagar por los productos es de: {total(pro1,pro2,pro3)}");
+
+
+
+double total(double pro1, double pro2, double pro3)
+{
+    return pro3 + pro2 + pro1;
+}
