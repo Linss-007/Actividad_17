0284d3ee20f7f3e552a370f6dcac893f5213fc49
Linss-007 <cristopheradl7@gmail.com>
2026-03-12 22:56:35 -0600

    Ejercicio 10

---
+++ 10-cubo/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Ingrese numero");
+double numero = double.Parse(Console.ReadLine());
+Console.WriteLine($"El cubo del numero es de: {cubo(numero)}");
+
+
+double cubo(double numero)
+{
+    return (numero * numero) * numero;
+}
