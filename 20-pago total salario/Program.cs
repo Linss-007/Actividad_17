0d318aa8b51f6110782b650534f37395548088b4
Linss_007 <cristopheradl7@gmail.com>
2026-03-13 10:03:54 -0600

    Ejercicio 20

---
+++ 20-pago total salario/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Ingrese cantidad de dias trabajados");
+int dias = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el pago por dia");
+double pagoDia = double.Parse(Console.ReadLine());
+Console.WriteLine($"Su salario total es de: {salario(dias,pagoDia)}");
+
+
+
+double salario(double dias, double pagoDia)
+{
+    return dias * pagoDia;
+}
