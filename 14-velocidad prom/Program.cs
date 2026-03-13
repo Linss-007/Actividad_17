bd59e2fd90a873fcd2109eb16dd7e23b7d51ed71
Linss_007 <cristopheradl7@gmail.com>
2026-03-13 08:32:25 -0600

    Ejercicio 14

---
+++ 14-velocidad prom/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Ingrese la distancia recorrida");
+double distancia = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el tiempo tardado");
+double tiempo = double.Parse(Console.ReadLine());
+Console.WriteLine($"La rapidez es de: {velocidad(distancia,tiempo)}");
+
+
+double velocidad(double distacia, double tiempo)
+{
+    return distacia / tiempo;
+}
