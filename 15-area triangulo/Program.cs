0c1cb104888639d3f4ddd0e9d5b341d2ee532044
Linss_007 <cristopheradl7@gmail.com>
2026-03-13 08:56:15 -0600

    Ejercicio 15

---
+++ 15-area triangulo/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Ingrese la base del triangulo");
+double baseTri = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese la altura del triangulo");
+double alturaTri = double.Parse(Console.ReadLine());
+Console.WriteLine($"El area del triangulo es de: {areaTri(baseTri, alturaTri)}");
+
+
+double areaTri(double baseTri, double alturaTri)
+{
+    double area = 0.5 * (baseTri * alturaTri);
+    return area;
+}
