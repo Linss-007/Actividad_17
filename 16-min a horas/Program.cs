c40d37150e4d5e4f1e0415b6a94343c54fd49a17
Linss_007 <cristopheradl7@gmail.com>
2026-03-13 09:00:18 -0600

    Ejercicio 16

---
+++ 16-min a horas/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Ingrese los minutos");
+int minutos = int.Parse(Console.ReadLine());
+Console.WriteLine($"El resultado de los minutos a horas es de: {minHora(minutos)}");
+
+
+double minHora(double minutos)
+{
+    return minutos/60;
+}
