f8f92799448e16f84b0e6ff8d6164cb480b73d61
Linss-007 <cristopheradl7@gmail.com>
2026-03-12 22:29:41 -0600

    Ejercicio 6

---
+++ 6-Celsius a Farenheit/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Ingrese los grados Celsius");
+double celsius = double.Parse(Console.ReadLine());
+Console.WriteLine($"El resultado en Farenheit es de: {celFarenheit(celsius)}");
+
+double celFarenheit(double celsius)
+{
+    return (celsius * 1.8) + 32;
+}
